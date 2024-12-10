79ece95b57eba955534fafdcc91307bb21376c8b
JagodaBodnar <jagodabodnar90@gmail.com>
2024-12-10 17:13:53 +0100

    solve 1 task in advent of code challenge

---
+++ src/main/java/org/jsonpath/adventofcode_1/AdventOfCode_1.java
@@ -0,0 +1,54 @@
+package org.jsonpath.adventofcode_1;
+
+
+import java.util.List;
+import java.util.Objects;
+
+public class AdventOfCode_1 {
+    public static String filePath = "src/main/java/org/jsonpath/adventofcode_1/adventOfCode_1Input";
+    public static List<Integer> firstList;
+    public static List<Integer> secondList;
+
+    public static void main(String[] args) {
+        AdventOfCode_1 adventOfCode1 = new AdventOfCode_1();
+        // Read input from file and assign to 2 lists;
+        firstList = ReadFile.readListFromFile(filePath, 0);
+        secondList = ReadFile.readListFromFile(filePath, 1);
+        // Sort lists;
+        List<Integer> sortedFirstList = adventOfCode1.sortList(firstList);
+        List<Integer> sortedSecondList = adventOfCode1.sortList(secondList);
+
+        //    --- Day 1: Historian Hysteria part1 ---
+        int totalDistanceBetweenLists = adventOfCode1.compareNumbersAndSumDiff(sortedFirstList, sortedSecondList);
+        System.out.println("totalDistanceBetweenLists = " + totalDistanceBetweenLists); //2192892;
+
+        //    --- Day 1: Historian Hysteria part2 ---
+        int similarityScore = adventOfCode1.findSimilarityScore();
+        System.out.println("similarityScore = " + similarityScore); //22962826;
+    }
+
+    public List<Integer> sortList(List<Integer> list) {
+        list.sort(Integer::compare);
+        return list;
+    }
+
+    public int compareNumbersAndSumDiff(List<Integer> list1, List<Integer> list2) {
+        int sum = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            sum += Math.abs(list2.get(i) - list1.get(i));
+        }
+        return sum;
+    }
+
+    public int findSimilarityScore() {
+        int sum = 0;
+        for (Integer integer : firstList) {
+            sum += Math.abs(integer * findNumberOfRepetitions(integer));
+        }
+        return sum;
+    }
+
+    public int findNumberOfRepetitions(Integer number) {
+        return secondList.stream().filter(el -> Objects.equals(el, number)).toList().size();
+    }
+}
---
+++ src/main/java/org/jsonpath/adventofcode_1/ReadFile.java
@@ -0,0 +1,24 @@
+package org.jsonpath.adventofcode_1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReadFile {
+    public static List<Integer> readListFromFile(String filePath, int listNumber) {
+        List<Integer> list = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for (String line : lines) {
+                String[] split = line.split("   ");
+                list.add(Integer.parseInt(split[listNumber]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+}
